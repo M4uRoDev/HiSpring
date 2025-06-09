c22450bb7d9e07bea9ebedca1a124d70ea84c564
M4uRoDev <mauro.poblete.c@gmail.com>
2025-06-09 15:51:11 -0400

    Show data in console

--- src/main/java/cl/coredevx/writer/ConsoleItemWriter.java
+++ src/main/java/cl/coredevx/writer/ConsoleItemWriter.java
@@ -7,8 +7,12 @@ import org.springframework.batch.item.ItemWriter;
 import java.util.List;
 
 public class ConsoleItemWriter implements ItemWriter<Persona> {
+
     @Override
     public void write(Chunk<? extends Persona> chunk) {
-        chunk.forEach(System.out::println);
+        for (Persona persona : chunk) {
+            System.out.printf("ID: %d | Nombre: %s | Correo: %s%n",
+                    persona.getId(), persona.getNombre(), persona.getCorreo());
+        }
     }
 }
